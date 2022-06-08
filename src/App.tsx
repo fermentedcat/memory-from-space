f96af8e74c782e31f803d33c3fbaf163ae6ae03c
Maja Thunberg <majaneko@gmail.com>
2022-06-08 20:47:19 +0200

    Duplicate fetched images

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { Image } from './models/image'
+import { CardList } from "./components/CardList/CardList";
 
-const IMAGE_AMOUNT = 6
+const IMAGE_AMOUNT = 8
 const API_KEY = process.env.REACT_APP_API_KEY as string
 const API_URL = `https://api.nasa.gov/planetary/apod?count=${IMAGE_AMOUNT}&api_key=${API_KEY}`
 
@@ -15,7 +16,7 @@ function App() {
         if (res.statusText !== 'OK') {
           throw new Error('Failed to fetch images!')
         }
-        setImages(res.data)
+        setImages([...res.data, ...res.data])
       })
       .catch(err => {
         alert(err.message)
@@ -25,9 +26,7 @@ function App() {
   return (
     <div>
       {/* click button to play // load images */}
-      {images.map(image => (
-        <p key={image.title}>{image.title}</p>
-      ))}
+      <CardList images={images} />
     </div>
   );
 }
