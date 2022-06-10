bb8e1fefd7c751593dd8e3ea16f7a382daf7ea91
Maja Thunberg <majaneko@gmail.com>
2022-06-10 12:45:24 +0200

    Add ability to start / restart game

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,8 @@ import { GameContextInterface } from "./models/game-state";
 import { Image } from './models/image'
 import { CardList } from "./components/CardList/CardList";
 import { Modal } from "./components/Modal/Modal";
+import { Button } from "./components/Button/Button";
+import styles from './App.module.scss'
 
 const IMAGE_AMOUNT = 8
 const API_KEY = process.env.REACT_APP_API_KEY as string
@@ -14,7 +16,8 @@ const API_URL = `https://api.nasa.gov/planetary/apod?thumbs=True&count=${IMAGE_A
 
 function App() {
   const [images, setImages] = useState<Image[]>([]);
-  const { isFinished, attempts } = useContext(GameContext) as GameContextInterface;
+  const [showModal, setShowModal] = useState<boolean>(false);
+  const { isFinished, attempts, reset } = useContext(GameContext) as GameContextInterface;
 
   function fetchImages() {
     axios.get<Image[]>(API_URL)
@@ -28,20 +31,62 @@ function App() {
         alert(err.message)
       })
   }
-  
-  useEffect(() => {
-    fetchImages()
-  }, [])
 
+  function handleCloseModal() {
+    setShowModal(false)
+  }
+  
+  function handleStartGame() {
+    reset()
+    setShowModal(false)
+    fetchImages()
+  }
+
+  useEffect(() => {
+    let timer: NodeJS.Timer
+    if (isFinished) {
+      timer = setTimeout(() => {
+        setShowModal(true)
+      }, 1000);
+    }
+    return () => clearTimeout(timer)
+  }, [isFinished])
+  
   return (
-    <div>
-      {/* click button to play // load images */}
-      <CardList images={images} />
-      <Modal>
-        <p>Finished: {isFinished.toString()}</p>
-        <p>{attempts} attempts</p>
-      </Modal>
-    </div>
+    <main className={styles.container}>
+      {!!images.length ? (
+        <>
+          <CardList images={images} />
+          {isFinished && (
+            <div className={styles.startWrapper}>
+              <Button 
+                text="New game"
+                variant="confirm"
+                onClick={handleStartGame} 
+              />
+            </div>
+          )}
+        </>
+      ) : (  
+        <div className={styles.startWrapper}>
+          <Button 
+            text="Start game"
+            variant="confirm"
+            onClick={handleStartGame} 
+          />
+        </div>
+      )}
+      {showModal && (
+        <Modal
+          heading="You did it!"
+          body={[`You finished the game in ${attempts} attempts.`, 'Try again?']}
+          cta={[
+            <Button text="Start new game" variant="confirm" onClick={handleStartGame}/>,
+            <Button text="Cancel" variant="cancel" onClick={handleCloseModal}/>
+          ]}
+        />
+      )}
+    </main>
   );
 }
 
