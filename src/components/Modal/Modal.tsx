dfe22ad615a7079e7329cd67de9e8ebb30e0aa54
Maja Thunberg <majaneko@gmail.com>
2022-06-10 12:43:25 +0200

    Complete modal functionality & add styling

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,13 +1,46 @@
 import React from 'react'
+import { Button } from '../Button/Button'
+import styles from './Modal.module.scss'
 
 interface ModalProps {
-  children: React.ReactElement[]
+  heading?: string
+  body?: string[]
+  cta?: React.ReactElement[]
+  onClose?: () => void
+  children?: React.ReactElement[]
 }
 
-export const Modal: React.FC<ModalProps> = ({ children }) => {
+export const Modal: React.FC<ModalProps> = ({ 
+  heading, 
+  body, 
+  cta,
+  onClose,
+  children 
+}) => {
   return (
-    <div>
-      {children}
+    <>
+    <div className={styles.backdrop}></div>
+    <div className={styles.wrapper}>
+      <aside className={styles.modal}>
+        <div className={styles.mainContent}>
+          {heading && <h1>{heading}</h1>}
+          {body && body.map((text, index) => {
+            return <p key={index}>{text}</p>
+          })}
+          {children && children}
+        </div>
+        {cta && (
+          <div className={styles.actions}>
+            {cta}
+          </div>
+        )}
+        {onClose && (
+          <div className={styles.actions}>
+            <Button text="Close" variant="confirm" onClick={onClose} />
+          </div>
+        )}
+      </aside>
     </div>
+    </>
   )
 }
